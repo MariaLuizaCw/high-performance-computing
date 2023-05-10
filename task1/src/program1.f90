e27e3b22ac3465b8f75a4fe2fdd51d2c647d97d0
root <root@DESKTOP-O1197EE.localdomain>
2023-05-09 22:36:29 -0300

    generate programs

---
+++ task1/src/program1.f90
@@ -0,0 +1,54 @@
+PROGRAM MATRIX_MULTIPLICATION
+    IMPLICIT NONE
+    INTEGER :: i, j, n, invert
+    CHARACTER(len = 40) :: n_char
+    CHARACTER(len = 1) :: invert_char
+    REAL :: start_time, end_time, time_elapsed
+    REAL, DIMENSION(:,:), ALLOCATABLE :: A
+    REAL, DIMENSION(:), ALLOCATABLE :: b, c
+  
+    ! Recebe o tamanho da matriz  e se o loop deve ser invertido como parâmetro do usuário
+    call getarg(1,n_char)
+    call getarg(2,invert_char)
+
+    read(n_char,*) n
+    read(invert_char,*) invert
+
+    ALLOCATE(A(n,n))
+    ALLOCATE(b(n))
+    ALLOCATE(c(n))
+  
+    ! Inicializa as matrizes com valores aleatórios e o vetor c em 0
+    CALL RANDOM_NUMBER(A)
+    CALL RANDOM_NUMBER(b)
+    c = 0.0
+
+    ! Inicia a contagem de tempo
+    CALL CPU_TIME(start_time)
+  
+    ! Realiza a multiplicação da matriz pelo vetor
+    IF (invert == 1) THEN
+        DO i = 1, n
+            DO j = 1, n
+            c(i) = c(i) + A(i,j) * b(j)
+            END DO
+        END DO
+    ELSE
+        DO j = 1, n
+            DO i = 1, n
+                c(i) = c(i) + A(i,j) * b(j)
+            END DO
+        END DO
+    END IF
+    ! Finaliza a contagem de tempo
+    CALL CPU_TIME(end_time)
+    time_elapsed = end_time - start_time
+
+    WRITE(*,*) 'Tempo de execução:', time_elapsed, 'segundos'
+  
+
+    DEALLOCATE(A)
+    DEALLOCATE(b)
+    DEALLOCATE(c)
+  
+  END PROGRAM MATRIX_MULTIPLICATION
---
+++ task1/src/program2.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void matriz_por_vetor_por_linha(float **matriz, float *vetor, float *resultado, int n) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            resultado[i] += matriz[i][j] * vetor[j];
+        }
+    }
+}
+
+
+
+void matriz_por_vetor_por_coluna(float **matriz, float *vetor, float *resultado, int n) {
+    for(int j = 0; j < n; j++) {
+        for(int i = 0; i < n; i++) {
+            resultado[i] += matriz[i][j] * vetor[j];
+        }
+    }
+   
+}
+
+int main(int argc, char **argv) {
+    int n =  atoi(argv[1]);
+    int inverter = atoi(argv[1]);
+    // Alocando a matriz
+    float **matriz = malloc(n * sizeof(float *));
+    for(int i = 0; i < n; i++) {
+        matriz[i] = malloc(n * sizeof(float));
+        for(int j = 0; j < n; j++) {
+            matriz[i][j] = (float)rand()/(float)(RAND_MAX/10);
+        }
+    }
+    
+    // Alocando o vetor
+    float *vetor = malloc(n * sizeof(float));
+    for(int i = 0; i < n; i++) {
+        vetor[i] = (float)rand()/(float)(RAND_MAX/10); 
+    }
+    
+    // Alocando o vetor resultado
+    float *resultado = malloc(n * sizeof(float));
+    for(int i = 0; i < n; i++) {
+        resultado[i] = 0; 
+    }
+    
+    // Multiplicando a matriz pelo vetor e calculando o tempo
+    clock_t start = clock();
+    if (inverter == 1){
+        matriz_por_vetor_por_coluna(matriz, vetor, resultado, n);
+    } else {
+
+        matriz_por_vetor_por_linha(matriz, vetor, resultado, n);
+    }
+    clock_t end = clock();
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    
+    printf("Tempo de execucao: %f segundos\n", time_spent);
+    
+    // Liberando a memória alocada
+    for(int i = 0; i < n; i++) {
+        free(matriz[i]);
+    }
+    free(matriz);
+    free(vetor);
+    free(resultado);
+    
+    return 0;
+}
